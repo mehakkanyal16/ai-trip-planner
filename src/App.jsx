6f9ea4c9532681c46ff96dd3d8daad228b7ec805
mehak <mehakkanyal@gmail.com>
2025-02-21 20:33:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hero from "./components/custom/Hero.jsx";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/*Hero*/}
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,32 @@
+import "react";
+import {useState} from 'react';
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+
+function CreateTrip() {
+    const [place,setPlace]=useState();
+  return (
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
+      <h2 className=" font-bold text-3xl">Tell us your Travel Preferences</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preferences.
+      </p>
+      <div className="mt-20">
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            What is destination of choice?
+          </h2>
+          <GooglePlacesAutocomplete apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY} />
+          {/* selectProps={{
+            place,
+            onChange:(v)=>setPlace(v);
+            console.log(v);
+
+          }} */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateTrip;
